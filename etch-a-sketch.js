10acb2020d5cc03d9ffecf377c0b836d5dc437e1
Bryan <109005501+bryanpina@users.noreply.github.com>
2023-11-02 16:58:08 +0900

    ADDED PROMPT FUNCTION AND BUTTON FUNCTIONALITY Prompt function is to select grid size and an event listener was added to the select size button to get value from user.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,20 @@
 // Code that creates a 16x16 grid of square divs
 
 const container = document.querySelector('.container');
+const btnSize = document.querySelector('.size');
+
+// When btn is clicked a prompt is given to the user
+
+btnSize.addEventListener('click', inputGridSize)
+
+// Asks for grid size from user and returns value
+
+function inputGridSize(){
+    return prompt("Please select canvas size.");
+}
+
 // loop that creates 256 divs (16x16)
+
 for(let i = 0; i < 256; i++) {
     const divs = document.createElement('div');
     divs.classList.add('items');
