7d3298b7995276935195345e208afecacefe4ca9
Bryan <109005501+bryanpina@users.noreply.github.com>
2023-11-03 11:29:14 +0900

    ADDED CSS GRID
    Added a css grid layout based on user prompt input as well as messages
    that output when a number less than 2 or greater than 100

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,22 +1,51 @@
-// Code that creates a 16x16 grid of square divs
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('hi')
+})
 
 const container = document.querySelector('.container');
 const btnSize = document.querySelector('.size');
+const message = document.querySelector('.message');
 
 // When btn is clicked a prompt is given to the user
 
 btnSize.addEventListener('click', inputGridSize)
 
-// Asks for grid size from user and returns value
+// Removes divs and adds divs based on users input via prompt
 
-function inputGridSize(){
-    return prompt("Please select canvas size.");
-}
+function inputGridSize(size){
 
-// loop that creates 256 divs (16x16)
+    // Removes previous divs when select size button is clicked
 
-for(let i = 0; i < 256; i++) {
-    const divs = document.createElement('div');
-    divs.classList.add('items');
-    container.appendChild(divs);
+    while(container.lastElementChild){
+        container.removeChild(container.lastElementChild);
+    }
+
+    //Prompts user to select canvas size
+
+    size = prompt("Please select canvas size.");
+    
+    if(size == ""){
+        message.innerText = 'Please provide a number from 2 to 100';
+    } else if(size < 2 || size > 100){
+        message.innerText = 'Only numbers from 2 to 100 are accepted';
+    } else{
+        message.innerText = 'Etch-A-Sketch';
+    }
+
+    //creates css grid layout based on size input
+    
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    // variable that multiples for columns and rows
+
+    let numDivs = size * size;
+
+    //loop that creates divs on grid based on number of divs
+
+    for(let i = 0; i < numDivs; i++) {
+        const divs = document.createElement('div');
+        divs.classList.add('items');
+        container.appendChild(divs);
+    }
 }
